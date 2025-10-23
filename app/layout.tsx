cf137efb35cc1b2a52845223587c3daf95b694cd
devarktini <arktiniinnovations.com>
2025-10-23 17:00:21 +0530

    icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ import ThemeProvider from '../Components/Components/ThemeProvider'
 export const metadata = {
   title: 'Book My Chef',
   description: 'Book your personal chef',
+  icons: {
+    icon: [
+      {
+        url: 'https://res.cloudinary.com/dzvvb0z0h/image/upload/f_auto,q_auto/v1757953170/removeb_sxbskt.png',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: 'https://res.cloudinary.com/dzvvb0z0h/image/upload/f_auto,q_auto/v1757953170/removeb_sxbskt.png',
+        type: 'image/png',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
