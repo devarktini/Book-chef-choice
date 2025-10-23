4b2ddac74c7e8d7b763ef4107d66e3dd1eb5404f
devarktini <arktiniinnovations.com>
2025-10-23 17:15:00 +0530

    code added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense, lazy } from "react";
-import HeroSection from "../components/Home/Hero/HeroSection"; // keep Hero direct since it’s first paint
+// import HeroSection from "../components/Home/Hero/HeroSection"; // keep Hero direct since it’s first paint
 
 // import SeventhSection from "../components/Home/Seventh Section/SeventhSection";
 
 // Lazy-loaded sections
+const HeroSection = lazy(() => import("../Components/Home/Hero/HeroSection"));
 const FirstSection = lazy(() => import("../Components/Home/FirstSection/FirstSection"));
-const SecondSection = lazy(() => import("../components/Home/SecondSection/SecondSection"));
+const SecondSection = lazy(() => import("../Components/Home/SecondSection/SecondSection"));
 const ThirdSection = lazy(() => import("../Components/Home/ThirdSection/ThirdSection"));
 const Fourth = lazy(() => import("../Components/Home/FourthSection/Fourth"));
 const Fifth = lazy(() => import("../Components/Home/FifthSection/Fifth"));
